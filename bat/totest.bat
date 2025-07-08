f925a4f54fe10e094756d922b3452d123b8e9277
盛祥辉 <294222163@qq.com>
2025-07-08 17:18:07 +0800

    修复脚本中的确认逻辑和延迟变量展开问题

--- bat/totest.bat
+++ bat/totest.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 :: Get current branch
 for /f "tokens=*" %%i in ('git rev-parse --abbrev-ref HEAD') do set CURRENT_BRANCH=%%i
-if "%CURRENT_BRANCH%"=="" (
+if "!CURRENT_BRANCH!"=="" (
     echo Error: Cannot get current branch
     exit /b 1
 )
@@ -16,7 +16,7 @@ if %ERRORLEVEL%==0 (
 )
 
 :: Confirm current branch is not test
-if "%CURRENT_BRANCH%"=="test" (
+if "!CURRENT_BRANCH!"=="test" (
     echo Error: Already on test branch, cannot merge
     exit /b 1
 )
@@ -32,7 +32,7 @@ if %ERRORLEVEL% neq 0 (
 echo Will perform the following operations:
 echo 1. Switch to test branch
 echo 2. Pull latest code from test branch
-echo 3. Merge %CURRENT_BRANCH% to test branch
+echo 3. Merge !CURRENT_BRANCH! to test branch
 echo 4. Push test branch to remote
 echo 5. Return to current branch
 set /p CONFIRM=Confirm to continue? (y/n):
@@ -54,17 +54,17 @@ echo Pulling latest code from test branch...
 git pull origin test
 if %ERRORLEVEL% neq 0 (
     echo Error: Failed to pull test branch
-    git checkout %CURRENT_BRANCH%
+    git checkout !CURRENT_BRANCH!
     exit /b 1
 )
 
 :: Merge current branch to test
-echo Merging %CURRENT_BRANCH% to test branch...
-git merge %CURRENT_BRANCH% --no-ff
+echo Merging !CURRENT_BRANCH! to test branch...
+git merge !CURRENT_BRANCH! --no-ff
 if %ERRORLEVEL% neq 0 (
     echo Error: Merge failed, there might be conflicts. Please resolve manually
     echo Use 'git merge --abort' to cancel the merge
-    git checkout %CURRENT_BRANCH%
+    git checkout !CURRENT_BRANCH!
     exit /b 1
 )
 
@@ -73,16 +73,16 @@ echo Pushing test branch to remote...
 git push origin test
 if %ERRORLEVEL% neq 0 (
     echo Error: Failed to push test branch
-    git checkout %CURRENT_BRANCH%
+    git checkout !CURRENT_BRANCH!
     exit /b 1
 )
 
 :: Return to original branch
-echo Returning to %CURRENT_BRANCH% branch...
-git checkout %CURRENT_BRANCH%
+echo Returning to !CURRENT_BRANCH! branch...
+git checkout !CURRENT_BRANCH!
 if %ERRORLEVEL% neq 0 (
-    echo Warning: Failed to return to %CURRENT_BRANCH% branch, please switch manually
+    echo Warning: Failed to return to !CURRENT_BRANCH! branch, please switch manually
 )
 
-echo Complete: Successfully merged %CURRENT_BRANCH% to test branch and pushed
+echo Complete: Successfully merged !CURRENT_BRANCH! to test branch and pushed
 exit /b 0
